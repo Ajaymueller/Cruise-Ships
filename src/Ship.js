a04db3fbc3ad4e4c1e3f84c970b306dd50261ab4
Aidan Mueller <Aidan_Mueller@hotmail.co.uk>
2020-04-15 21:34:04 +0100

    project set up

--- src/Ship.js
+++ src/Ship.js
@@ -1,4 +1,5 @@
-function Ship(itinerary) {
+(function exportShip() {
+  function Ship(itinerary) {
   this.itinerary = itinerary;
   this.currentPort = itinerary.ports[0];
   this.previousPort = null;
@@ -22,6 +23,9 @@ Ship.prototype = {
     this.currentPort.addShip(this);
   },
 };
-
-
-module.exports = Ship;
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = Ship;
+} else {
+  window.Ship = Ship;
+}
+}());
